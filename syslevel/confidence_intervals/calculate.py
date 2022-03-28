4357a9b30bd0ba0602c22516ea8f0c3d21b48cff
Daniel Deutsch <danfdeutsch@gmail.com>
2022-03-28 10:41:31 -0400

    Reducing sampling size

--- syslevel/confidence_intervals/calculate.py
+++ syslevel/confidence_intervals/calculate.py
@@ -21,6 +21,7 @@ def main(args):
             coefficient,
             args.resampling_method,
             paired_inputs=paired_inputs,
+            n_resamples=1000
         )
 
         with open(f"{args.output_dir}/{coefficient}.txt", "w") as out:
